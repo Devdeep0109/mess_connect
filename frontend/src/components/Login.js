8eca76fec5076152104a368e581517b9fe5d57ab
Devdeep0109 <devdeepgoyal@gmail.com>
2024-03-01 16:39:51 +0530

    first commit

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const LoginPage = () =>{
+    return (
+        <div>
+            <h1>Login Page</h1>
+            <form action="">
+                <label htmlFor="">First Name</label>
+                <input type="text" />
+                <label htmlFor="">Last Name</label>
+                <input type="text" />
+
+                <label htmlFor="">Enter password</label>
+                <input type="password" />
+
+
+            </form>
+        </div>
+    )
+}
+export default LoginPage;
---
+++ frontend/src/components/Register.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const RegisterPage = () =>{
+    return (
+        <div>
+            <h1>Register Page</h1>
+            <form action="">
+                <label htmlFor="">Full Name</label>
+                <input type="text" />
+                <label htmlFor="">Last Name</label>
+                <input type="text" />
+                <label htmlFor="">Email Id</label>
+                <input type="email" />
+
+                <label htmlFor="">Enter password</label>
+                <input type="password" />
+
+                <label htmlFor="">Re-Enter password</label>
+                <input type="password" />
+
+            </form>
+        </div>
+    )
+}
+
+export default RegisterPage;
